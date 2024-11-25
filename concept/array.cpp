02f24a55ad069358787091b493ce1f903f7f1311
sexyseunghwan <ssh9308@naver.com>
2024-11-25 22:15:55 +0900

    2024-11-25
    2024-11-25

---
+++ concept/array.cpp
@@ -0,0 +1,29 @@
+// g++ -std=c++20 dfs.cpp -o dfs
+#include <iostream>
+
+
+using namespace std;
+
+
+void array_0();
+
+
+int main() {
+
+    
+    
+}
+
+
+void array_0() {
+    
+    /* 배열 초기화 하는 방법 */
+    int arr[5] = {1,2,3,4,5};
+
+
+    for (int i = 0; i < 5; i++) {
+        
+    }
+
+
+}
