ab015587c1e725e482696ab63e1b153a6a019939
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2024-12-18 17:57:12 +0900

    2024-12-18
    2024-12-18

---
+++ test/2178/2178.cpp
@@ -0,0 +1,21 @@
+// g++ -std=c++20 2178.cpp -o 2178
+// g++ -std=c++17 2178.cpp -o 2178
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N,M;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    
+
+
+
+    return 0;
+}
